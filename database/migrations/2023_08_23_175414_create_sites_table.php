92b02295bb856da9d007c59ad2ef4ee4079c9d94
Rafaele <rafaelegolfeto@gmail.com>
2023-08-25 12:57:26 -0300

    feat: create model and table sites

---
+++ database/migrations/2023_08_23_175414_create_sites_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sites', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('url');
+            $table->string('user_id')->index();
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                    ->references('id')
+                    ->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sites');
+    }
+};
