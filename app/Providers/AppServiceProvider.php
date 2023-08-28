ee352bc44ee278e05a3ca5d9bfd4f45da16fe5d4
Rafaele <rafaelegolfeto@gmail.com>
2023-08-28 16:24:40 -0300

    feat: create observer check model

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Check;
+use App\Observers\CheckObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        Check::observe(CheckObserver::class);
     }
 
     /**
@@ -19,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Check::observe(CheckObserver::class);
     }
 }
