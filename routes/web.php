f6a8f800570d0905feeac6f79e8f94e081da99a2
Rafaele <rafaelegolfeto@gmail.com>
2023-08-28 14:59:04 -0300

    feat: show logs endpoints checks

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,20 @@
 <?php
 
+use App\Http\Controllers\Admin\CheckController;
 use App\Http\Controllers\Admin\EndpointController;
 use App\Http\Controllers\Admin\SiteController;
 use App\Http\Controllers\ProfileController;
 use App\Models\Endpoint;
 use Illuminate\Support\Facades\Route;
 
+
 Route::middleware(['auth'])
     ->prefix('admin')
     ->group(function() {
+    Route::get('/endpoints/{endpoint}/logs', [CheckController::class, 'index'])->name('endpoints.checks');
+
     Route::resource('sites/{site}/endpoints', EndpointController::class);
+
     Route::delete('/sites/{site}', [SiteController::class, 'destroy'])->name('sites.destroy');
     Route::put('/sites/{site}', [SiteController::class, 'update'])->name('sites.update');
     Route::get('/sites/{site}/edit', [SiteController::class, 'edit'])->name('sites.edit');
